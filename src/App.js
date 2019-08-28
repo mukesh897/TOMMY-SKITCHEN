cd704a25085c599275454ca0be5f01821720fd06
mukesh <mukesh.bhatia85@gmail.com>
2019-08-28 17:30:41 +0530

    ComingSoon

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo192.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}  alt="logo" />
+        <p>
+          TOMMY'S KITCHEN
+        </p>
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          COMING SOON
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
